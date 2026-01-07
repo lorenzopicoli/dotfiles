66bb79043c4423f6d452730067a6b83af300a050
Lorenzo <lorenzopicoli@me.com>
2026-01-06 21:28:25 -0300

    fix binstall

--- bootstrap/programs/rust.sh
+++ bootstrap/programs/rust.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
 if command_exists cargo; then
-  log "rust/cargo already installed"
-  return
-fi
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+  log "cargo already installed"
+else
+  log "installing rust/cargo"
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
 
-# Make cargo available *now*, without shell reload
-if [ -f "$HOME/.cargo/env" ]; then
-  source "$HOME/.cargo/env"
+  # Make cargo available immediately (no shell reload)
+  if [ -f "$HOME/.cargo/env" ]; then
+    source "$HOME/.cargo/env"
+  fi
+fi
+
+# Install cargo-binstall only if it's not available
+if cargo binstall --help >/dev/null 2>&1; then
+  log "cargo-binstall already installed"
+else
+  log "installing cargo-binstall"
+  curl -L --proto '=https' --tlsv1.2 -sSf \
+    https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh \
+    | bash
 fi
