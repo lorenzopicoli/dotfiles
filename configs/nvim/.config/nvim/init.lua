216d2cc9e10dbf7ce8c0210bb0f9e1b2278e2367
Lorenzo <lorenzopicoli@me.com>
2026-01-11 11:56:25 -0300

    nvim configuration

---
+++ configs/nvim/.config/nvim/init.lua
@@ -0,0 +1,5 @@
+require('configs')
+require('keymaps')
+require('autocmds')
+require('plugins')
+require('statusline')
---
+++ configs/nvim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,70 @@
+-- Avoids space being used in normal mode which causes some weirdness when using space as a leader
+vim.keymap.set("", "<Space>", "<Nop>")
+
+vim.g.mapleader = " "
+-- leader + PV opens explorer
+vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>", {
+	desc = "Open Oil (project view)",
+})
+-- File finder
+vim.keymap.set("n", "<leader>ff", function()
+	require("fzf-lua").files()
+end)
+-- File home. Look for file in all of home dir
+vim.keymap.set("n", "<leader>fh", function()
+	require("fzf-lua").files({
+		cwd = vim.env.HOME,
+		prompt = "Home ❯ ",
+		fd_opts = "--ignore-file ~/.config/fd/ignore",
+		hidden = false,
+	})
+end, { desc = "FZF: Search files in HOME" })
+-- File search
+vim.keymap.set("n", "<leader>fs", function()
+	require("fzf-lua").live_grep()
+end)
+
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+
+-- Make sure that default delete won't act as cut. So text won't go to clipboard
+vim.keymap.set("n", "d", '"_d')
+vim.keymap.set("v", "d", '"_d')
+-- Scroll half page with Control + J + "zz" which centers the buffer on the cursor. Makes scrolling simpler
+vim.keymap.set("n", "<C-J>", "<C-D>zz")
+-- Scrop up half a page with Control + K + "zz" which centers the buffer on the cursor. Makes scrolling simpler
+vim.keymap.set("n", "<C-K>", "<C-U>zz")
+-- Both in normal and visual mode, Shift + d acts as default d so it acts
+-- as cutting moving the text to clipboard
+vim.keymap.set("n", "D", "d")
+vim.keymap.set("n", "DD", "dd")
+vim.keymap.set("v", "D", "d")
+vim.keymap.set("v", "DD", "dd")
+
+vim.keymap.set("n", "cc", "gcc", { remap = true })
+vim.keymap.set("v", "cc", "gcc", { remap = true })
+vim.keymap.set("n", "gd", "gri", { remap = true })
+
+-- Code action if kind is quickfix
+vim.keymap.set({ "n", "x" }, "<leader>ca", function()
+	require("tiny-code-action").code_action({
+		filter = function(action, client)
+			local kind = action.kind
+
+			if kind and kind:find("quickfix", 1, true) then
+				return true
+			end
+
+			return false
+		end,
+	})
+end, { noremap = true, silent = true })
+-- Diagnostics
+vim.keymap.set("n", "H", "<cmd>lua vim.diagnostic.open_float()<CR>")
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	callback = function()
+		vim.wo.winhighlight = "Normal:Normal,NormalNC:NormalNC"
+	end,
+})
+
+vim.api.nvim_set_hl(0, "NormalNC", { link = "Comment" })
